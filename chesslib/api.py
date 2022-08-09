a1ea0bdca10430b5cd5a5c8407504fdc5cbb7fba
DanielH4 <danne.halvarsson@gmail.com>
2022-08-09 21:08:59 +0200

    implement comparison operators for chess games
    Implemented methods include: __eq__ and __ne__.

--- chesslib/api.py
+++ chesslib/api.py
@@ -208,3 +208,11 @@ class Chess():
             self._turn = 'black'
         else:
             self._turn = 'white'
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.toJSON() == other.toJSON()
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
